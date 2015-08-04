e694f13fbd5a9f29e886b05f0237a1ff4b79c24a
helenlin0621 <helen_lin0621@yahoo.com.tw>
2015-08-04 17:25:45 +0900

    local resources  html update

--- resources/views/manage_pages/people_manage_resource_l.blade.php
+++ resources/views/manage_pages/people_manage_resource_l.blade.php
@@ -9,10 +9,15 @@
 @section('content')
     <div class="col-xs-8 col-sm-6 col-md-6" >
     {{--1.增援人員需求 (人數、人員背景) 地方指揮官--}}
-    2.查看報到志工人員資料
-    <table class="table table-bordered">
-        <tr><th>姓名</th><th>電話</th><th>Email</th><th>所在地</th><th></th></tr>
-        <tr><td>王小明</td><td>0912312312</td><td>123yahoo.com.tw</td><td>目前地點</td><td><button class="btn btn-default">報到</button></td></tr>
+    {{--2.查看報到志工人員資料--}}
+    <table class="table table-striped">
+        <thead>
+            <tr><td colspan="2"><h5><b> 志工人員報到表</b></h5></td></tr>
+            <tr><th>姓名</th><th>電話</th><th>Email</th><th>所在地</th><th></th></tr>
+        </thead>
+        <tbody>
+            <tr><td>王小明</td><td>0912312312</td><td>123yahoo.com.tw</td><td>目前地點</td><td><button class="btn btn-default">報到</button></td></tr>
+        </tbody>
     </table>
     </div>
 @endsection
--- resources/views/manage_pages/product_manage_resource_l.blade.php
+++ resources/views/manage_pages/product_manage_resource_l.blade.php
@@ -6,188 +6,144 @@
     <li>{!! Html::link('resource/manage/product/local', '物資管理') !!}</li>
     <li>{!! Html::link('resource/manage/people/local', '人員管理') !!}</li>
 @endsection
+@section('css')
+    tr.header,
+    {
+    cursor:pointer;
+    }
+    .header .sign:after{
+    content:"▼";
+    display:inline-block;
+    }
+    .header.expand .sign:after{
+    content:"▲";
+    }
+
+@endsection
 @section('content')
-    1.看到目前物資存量
-    2.點收中央發放物資
-    3.物資發放(借用器材)
-    4.額外物資需求(增援)
+    {{--1.看到目前物資存量--}}
+    {{--2.點收中央發放物資--}}
+    {{--3.物資發放(借用器材)--}}
+    {{--4.額外物資需求(增援)--}}
     <!-- Nav tabs -->
     <ul class="nav nav-tabs" role="tablist">
-        <li><a href="#productStatus" role="tab" data-toggle="tab"><b>物資存量管理</b></a></li>
-        <li><a href="#donate" role="tab" data-toggle="tab"><b>民眾捐贈單管理</b></a></li>
-        <li class="active"><a href="#assign" role="tab" data-toggle="tab"><b>分配物資</b></a></li>
+        <li class="active"><a href="#productStatus" role="tab" data-toggle="tab"><b>物資存量管理</b></a></li>
+        <li><a href="#borrow" role="tab" data-toggle="tab"><b>器材借用</b></a></li>
 
     </ul>
     <br><br>
     <!-- Tab panes -->
-    <div class="tab-content">
-        <div class="tab-pane " id="productStatus">
+    <div class="tab-content ">
+        <div class="tab-pane active" id="productStatus">
             {{--<div class="col-xs-16 col-sm-12 col-md-12" >--}}
             {{--1.看到目前物資存量<br>--}}
             {{--2.新增修改需求捐贈數量--}}
             {{--</div>--}}
             <div class="col-xs-16 col-sm-12 col-md-12" >
-                <div class="col-xs-10 col-sm-7 col-md-7" >
+                <div class="col-xs-12 col-sm-8 col-md-8" >
                     <table class="table  table-striped">
                         {{--{!! Form::open(array('url' => 'call/manage/save', 'method' => 'post')) !!}--}}
                         <thead>
-                        <tr><td colspan="7"><h5><b> 地方物資存量列表</b></h5></td></tr>
-                        <tr><th width="10%">物資編號</th><th width="10%">名稱</th><th width="10%">目前存量</th><th width="10%">安全存量</th><th width="10%">單位</th></tr>
+                        <tr><td colspan="2"><h5><b> 地方物資存量列表</b></h5></td><th class="text-right">提領人</th><td colspan="3">{!! Form::text('leader', '', ['id' =>  'leader', 'placeholder' =>  '編號 或 姓名','class' => 'form-control ', 'required']) !!}</td></tr>
+                        <tr><th width="15%">物資編號</th><th width="15%">名稱</th><th width="15%">目前存量</th><th width="15%">安全存量</th><th width="10%">單位</th><th width="15%">提領數量</th></tr>
                         </thead>
                         <tbody>
 
-                        <tr><td>1</td><td>泡麵</td><td class="text-right">123</td><td class="text-right">345</td><td>包</td></tr>
+                        <tr><td>1</td><td>泡麵</td><td class="text-right">123</td><td class="text-right">345</td><td>包</td><td>{!! Form::number('name', 0,['class' => 'form-control text-right','min'=>'0','max'=>'100'])!!}</td></tr>
+                        <tr><td>2</td><td>礦泉水</td><td class="text-right">123</td><td class="text-right">345</td><td>包</td><td>{!! Form::number('name', 0,['class' => 'form-control text-right','min'=>'0','max'=>'100'])!!}</td></tr>
+                        <tr><td>3</td><td>繃帶</td><td class="text-right">123</td><td class="text-right">345</td><td>包</td><td>{!! Form::number('name', 0,['class' => 'form-control text-right','min'=>'0','max'=>'100'])!!}</td></tr>
 
+                        <tr><td colspan="5"></td><td class="text-right">{!! Form::submit('提領', ['class' => 'btn btn-default btn-sm']) !!}</td></tr>
                         </tbody>
                         {{--{!! Form::close() !!}--}}
                     </table>
                 </div>
-                <div class="col-xs-6 col-sm-5 col-md5" >
+                <div class="col-xs-4 col-sm-4 col-md-4" >
                     <table class="table  table-striped">
                         <thead>
-                        <tr><td colspan="6"><h5><b>物資需求列表(向民眾募集)</b></h5></td></tr>
+                        <tr><td colspan="6"><h5><b>中央發放物資列表</b></h5></td></tr>
 
-                        <tr><th>物資編號</th><th>物資名稱</th><th>需求數量</th><th>已認捐數量</th><th>單位</th><th>修改</th></tr>
+                        <tr><th>物資編號</th><th>物資名稱</th><th>發放數量</th><th>單位</th><th>實際點收數量</th></tr>
                         </thead>
                         <tbody>
 
-                        <tr><td>1</td><td>泡麵</td><td class="text-right">20</td><td class="text-right">10</td><td>包</td><td><button class="btn btn-link btn-sm" data-toggle="modal" data-target="#update">修改</button></td></tr>
-                        <!-- Modal -->
-                        <div class="modal fade" id="update" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                            <div class="modal-dialog">
-                                <div class="modal-content">
-                                    {{--{!! Form::open(array('url' => 'call/manage/createMission', 'method' => 'post','class' => 'form-horizontal','onSubmit' => 'return checkForm();')) !!}--}}
-                                    <div class="modal-header">
-                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                        <h4 class="modal-title" id="myModalLabel"><b>修改欲募捐數量</b></h4>
-                                    </div>
-                                    <div class="modal-body">
-                                        <dl class="dl-horizontal">
-                                            <dt>物資編號</dt>
-                                            <dd>1</dd><br>
-                                            <dt>物資名稱</dt>
-                                            <dd>泡麵</dd><br>
-                                            {!! Form::hidden('product_id','value') !!}
-                                            <dt>需求數量</dt>
-                                            <dd class="text-right">33</dd><br>
-                                            <dt>欲修改需求數量</dt>
-                                            <dd class="text-right">{!! Form::number('require_number','',['id' =>  'mission_list_name','class' => 'form-control text-right','min'=>'0']) !!}</dd> <br>
+                        <tr><td>1</td><td>泡麵</td><td class="text-right">20</td><td>包</td><td>{!! Form::number('name', 0,['class' => 'form-control text-right','min'=>'0','max'=>'100'])!!}</td></tr>
+                        <tr><td>1</td><td>泡麵</td><td class="text-right">20</td><td>包</td><td>{!! Form::number('name', 0,['class' => 'form-control text-right','min'=>'0','max'=>'100'])!!}</td></tr>
+                        <tr><td>1</td><td>泡麵</td><td class="text-right">20</td><td>包</td><td>{!! Form::number('name', 0,['class' => 'form-control text-right','min'=>'0','max'=>'100'])!!}</td></tr>
+                        <tr><td>1</td><td>泡麵</td><td class="text-right">20</td><td>包</td><td>{!! Form::number('name', 0,['class' => 'form-control text-right','min'=>'0','max'=>'100'])!!}</td></tr>
+                        <tr><td colspan="5" class="text-center">{!! Form::submit('點收完成', ['class' => 'btn btn-default btn-sm']) !!}</td></tr>
 
-                                        </dl>
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-default btn-sm" data-dismiss="modal">取消</button>
-                                        {!! Form::submit('修改', ['class' => 'btn btn-default btn-sm btn-primary']) !!}
-                                    </div>
-                                </div><!-- /.modal-content -->
-                                {!! Form::close() !!}
-                            </div><!-- /.modal-dialog -->
-                        </div><!-- /.modal -->
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
-        <div class="tab-pane" id="donate">
-            {{--<div class="col-xs-16 col-sm-12 col-md-12" >--}}
-            {{--3.點收捐贈單--}}
-            {{--</div>--}}
+
+        <div class="tab-pane" id="borrow">
 
             <div class="col-xs-16 col-sm-12 col-md-12" >
 
                 <div class="col-xs-12 col-sm-8 col-md-8" >
                     <table class="table">
-                        {{--{!! Form::open(array('url' => 'call/manage/save', 'method' => 'post')) !!}--}}
+
                         <thead>
-                        <tr><td colspan="7"><h5><b>捐贈單列表</b></h5></td></tr>
-                        <tr><th>捐贈單編號</th><th>捐贈人姓名</th><th>捐贈人電話</th><th>捐贈人Email</th><th></th></tr>
-                        </thead>
-                        <tbody>
-
-                        <tr class="header expand" style="background-color: #f9f9f9; border-top-width:3px; border-top-style:solid; border-top-color: #dddddd"><td>1</td><td>王小明</td><td>0912312312</td><td>123yahoo.com.tw</td><td><span class="sign"></span></td></tr>
-                        <tr><td></td><th>物資名稱</th><th>捐贈數量</th><th>單位</th><th>點收</th></tr>
-                        <tr><td></td><td>泡麵</td><td class="text-right">100</td><td>包</td><td>{!! Form::checkbox('name', 'value')!!}</td></tr>
-                        <tr><td></td><td>礦泉水</td><td class="text-right">2</td><td>瓶</td><td>{!! Form::checkbox('name', 'value')!!}</td></tr>
-                        <tr><td colspan="4"></td><td>{!! Form::submit('點收完成', ['class' => 'btn btn-default btn-sm']) !!}</td></tr>
-                        <tr class="header expand"  style="background-color: #f9f9f9; border-top-width:3px; border-top-style:solid; border-top-color: #dddddd"><td>1</td><td>王小明</td><td>0912312312</td><td>123yahoo.com.tw</td><td><span class="sign"></span></td></tr>
-                        <tr><td>物資名稱</td><td>捐贈數量</td></tr>
-
-                        </tbody>
-                        {{--{!! Form::close() !!}--}}
-                    </table>
-                </div>
-                {{--<div class="col-xs-4 col-sm-4 col-md-4" >--}}
-                {{--<table class="table  table-striped">--}}
-                {{--<thead>--}}
-                {{--<tr><td colspan="7"><h5><b>人員需求列表(中央決定派遣給任務)</b></h5></td></tr>--}}
-                {{--<tr><th>任務編號</th><th>需求人數</th><th>需求人員種類</th></tr>--}}
-                {{--</thead>--}}
-                {{--<tbody>--}}
-
-                {{--<tr><td>1</td><td>23</td><td>醫療組</td></tr>--}}
-
-                {{--</tbody>--}}
-                {{--</table>--}}
-
-                {{--</div>--}}
-            </div>
-        </div>
-
-        <div class="tab-pane active" id="assign">
-            {{--<div class="col-xs-16 col-sm-12 col-md-12" >--}}
-            {{--4.分配物資(至地方)<br>--}}
-            {{--4.1 看到各地方存量不足<br>--}}
-            {{--4.2 修改各地方安全存量(如有增援需求)<br>--}}
-            {{--</div>--}}
-
-            <div class="col-xs-16 col-sm-12 col-md-12" >
-
-                <div class="col-xs-12 col-sm-8 col-md-8" >
-                    <table class="table">
-                        {{--{!! Form::open(array('url' => 'call/manage/save', 'method' => 'post')) !!}--}}
-                        <thead>
-                        <tr><td colspan="8"><h5><b>地方物資存量表</b></h5></td></tr>
-                        <tr><th width="10%">任務編號</th><th colspan="2">任務名稱</th><th>負責人</th><th>負責人電話</th><th>負責人Email</th></tr>
+                        <tr><td colspan="7"><h5><b>地方器材借用表</b></h5></td></tr>
+                        <tr><th>器材編號</th><th colspan="2">器材名稱</th><th>器材總數</th><th>借出數量</th><th colspan="3" class="text-right">借用資料/歸還</th></tr>
                         </thead>
                         <tbody>
 
                         <tr class="header expand"  style="background-color: #f9f9f9; border-top-width:3px; border-top-style:solid; border-top-color: #dddddd">
-                            <td>1</td><td colspan="2">西屯區</td><td>陳芊蓉</td><td>0987654321</td><td>789yahoo.com.tw</td><td>警告:尚未送出物資</td><td><span class="sign"></span></td>
+                            <td>1</td><td colspan="2">電鑽</td><td class="text-right">3</td><td class="text-right">2</td><td class="text-right" colspan="3"><span class="sign"></span></td>
                         </tr>
-                        <tr><td width="10%"></td><th width="15%">物資編號</th><th width="15%">物資名稱</th><th width="15%">安全存量</th><th width="15%">目前存量</th><th width="5%">單位</th><th width="20%">指揮官分配數量</th><th width="5%"></th></tr>
-                        <tr><td></td><td>1</td><td>泡麵</td><td class="text-right">345</td><td class="text-right">123</td><td>包</td><td class="text-right">60</td><td>{!! Form::checkbox('name', 'value')!!}</td></tr>
-                        <tr><td></td><td>2</td><td>礦泉水</td><td class="text-right">345</td><td class="text-right">123</td><td>瓶</td><td class="text-right">50</td><td>{!! Form::checkbox('name', 'value')!!}</td></tr>
-                        <tr><td colspan="3"></td><td class="text-right">{!! Form::submit('修改安全存量', ['class' => 'btn btn-default btn-sm']) !!}</td><td colspan="2"></td><td colspan="2" class="text-right">{!! Form::submit('已送出物資', ['class' => 'btn btn-default btn-sm']) !!}</td></tr>
-
+                        <tr><th></th><th colspan="2">借用時間</th><th>借用人</th><th>借用數量</th><th>所在地</th><th colspan="2">歸還</th></tr>
+                        {{--{!! Form::open(array('url' => 'call/manage/save', 'method' => 'post')) !!}--}}
+                        <tr><td></td><td>2014/08/04</td><td>04:23:32</td><td>陳芊蓉</td><td class="text-right">2</td><td>資電館(?</td><td>{!! Form::number('name', 0,['class' => 'form-control text-right','min'=>'0','max'=>'100'])!!}</td><td>{!! Form::submit('歸還', ['class' => 'btn btn-default btn-sm']) !!}</td></tr>
+                        {!! Form::hidden('borrow','1') !!}
+                        {{--{!! Form::close() !!}--}}
                         <tr class="header expand"  style="background-color: #f9f9f9; border-top-width:3px; border-top-style:solid; border-top-color: #dddddd">
-                            <td>2</td><td colspan="2">北屯區</td><td>陳芊蓉</td><td>0987654321</td><td>789yahoo.com.tw</td><td></td><td><span class="sign"></span></td>
+                            <td>2</td><td colspan="2">工具箱</td><td class="text-right">3</td><td class="text-right">2</td><td class="text-right" colspan="3"><span class="sign"></span></td>
                         </tr>
-                        <tr><td width="10%"></td><th width="15%">物資編號</th><th width="15%">物資名稱</th><th width="15%">目前存量</th><th width="15%">安全存量</th><th width="5%">單位</th><th width="20%">中央指揮官分配數量</th><th width="5%"></th></tr>
-                        <tr><td></td><td>1</td><td>泡麵</td><td class="text-right">123</td><td class="text-right">345</td><td>包</td><td class="text-right">0</td><td>{!! Form::checkbox('name', 'value')!!}</td></tr>
-                        <tr><td></td><td>2</td><td>礦泉水</td><td class="text-right">123</td><td class="text-right">345</td><td>瓶</td><td class="text-right">0</td><td>{!! Form::checkbox('name', 'value')!!}</td></tr>
-                        <tr><td colspan="3"></td><td class="text-right">{!! Form::submit('修改安全存量', ['class' => 'btn btn-default btn-sm']) !!}</td><td  colspan="2"></td><td colspan="2" class="text-right">{!! Form::submit('已送出物資', ['class' => 'btn btn-default btn-sm']) !!}</td></tr>
+                        <tr><th></th><th colspan="2">借用時間</th><th>借用人</th><th>借用數量</th><th>所在地</th><th colspan="2">歸還</th></tr>
+                        {{--{!! Form::open(array('url' => 'call/manage/save', 'method' => 'post')) !!}--}}
+                        <tr><td></td><td>2014/08/04</td><td>04:23:32</td><td>陳芊蓉</td><td class="text-right">2</td><td>資電館(?</td><td>{!! Form::number('name', 0,['class' => 'form-control text-right','min'=>'0','max'=>'100'])!!}</td><td>{!! Form::submit('歸還', ['class' => 'btn btn-default btn-sm']) !!}</td></tr>
+                        {!! Form::hidden('borrow','1') !!}
+                        {{--{!! Form::close() !!}--}}
 
                         </tbody>
-                        {{--{!! Form::close() !!}--}}
+
                     </table>
                 </div>
-                {{--<div class="col-xs-4 col-sm-4 col-md-4" >--}}
-                {{--<table class="table  table-striped">--}}
-                {{--<thead>--}}
-                {{--<tr><td colspan="7"><h5><b>人員需求列表(中央決定派遣給任務)</b></h5></td></tr>--}}
-                {{--<tr><th>任務編號</th><th>需求人數</th><th>需求人員種類</th></tr>--}}
-                {{--</thead>--}}
-                {{--<tbody>--}}
+                <div class="col-xs-4 col-sm-4 col-md-4" >
+                    {{--{!! Form::open(array('url' => 'call/manage/save', 'method' => 'post')) !!}--}}
+                    <table class="table table-striped">
+                        <thead>
+                            <tr><th colspan="2"><h5><b>器材借用表單</b></h5></th></tr>
+                        </thead>
+                        <tbody>
+                            <tr><th>借用器材</th><td>{!! Form::select('name', array('1' => '器材1', '2' => '器材2', '3' => '器材3'), '2', ['class' => 'form-control']) !!}</td></tr>
+                            <tr><th>借用數量</th><td>{!! Form::number('require_number','',['id' =>  'mission_list_name','class' => 'form-control text-right', 'required','min'=>'0']) !!}</td></tr>
+                            <tr><th>借用人</th><td>{!! Form::text('leader', '', ['id' =>  'leader', 'placeholder' =>  '編號 或 姓名','class' => 'form-control ', 'required']) !!}</td></tr>
+                            <tr><td colspan="2" class="text-center">{!! Form::submit('借用', ['class' => 'btn btn-default btn-sm']) !!}</td></tr>
+                        </tbody>
+                    </table>
 
-                {{--<tr><td>1</td><td>23</td><td>醫療組</td></tr>--}}
 
-                {{--</tbody>--}}
-                {{--</table>--}}
-
-                {{--</div>--}}
+                    {{--{!! Form::close() !!}--}}
+                </div>
             </div>
         </div>
 
     </div>
 
+@endsection
+@section('javascript')
+
+    <script language="JavaScript">
+        $('.header').click(function(){
+            $(this).toggleClass('expand').nextUntil('tr.header').slideToggle(100);
+        });
+        $('.header').trigger('click'); //trigger :觸發指定事件
+
+
+    </script>
 @endsection
