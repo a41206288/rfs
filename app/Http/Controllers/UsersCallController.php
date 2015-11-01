e3a34d99992325cc64dd080b95e7db5b9c4e2039
CQR <sm29025292@gmail.com>
2015-11-01 16:54:44 +0800

    民眾-通報 信箱重複檢查 送出後頁面修改

--- app/Http/Controllers/UsersCallController.php
+++ app/Http/Controllers/UsersCallController.php
@@ -4,6 +4,11 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
+
+use App\Mission;
+use Carbon\Carbon;
 
 class UsersCallController extends Controller {
 
@@ -14,7 +19,14 @@ class UsersCallController extends Controller {
 	 */
 	public function index()
 	{
-        return view('user_pages.call_input');
+        $email_users = DB::table('users')->where('email','<>','null')->lists('email');
+        $email_support = DB::table('center_support_person_details')->where('email','<>','null')->lists('email');
+        $email_missions = DB::table('missions')->where('email','<>','null')->lists('email');
+        $send_email = array_merge($email_users, $email_support);
+        $send_email = array_merge($send_email, $email_missions);
+//        dd($send_email);
+
+        return view('user_pages.call_input')->with('email', $send_email);
 	}
 
 	/**
@@ -24,7 +36,54 @@ class UsersCallController extends Controller {
 	 */
 	public function create()
 	{
-        return view('user_pages.submit_success')->with('string',"向我們提供災情最新情況");
+        $township =Input::get( 'township' );
+        $street1 =Input::get( 'street1' );
+        $street2 =Input::get( 'street2' );
+        $adressDetail =Input::get( 'adressDetail' );
+        $remark =Input::get( 'remark' );
+        $lname =Input::get( 'lname' );
+        $fname =Input::get( 'fname' );
+        $sex =Input::get( 'sex' );
+        $phone =Input::get( 'phone' );
+        $email =Input::get( 'email' );
+//        dd($township."\n".$street1."\n".$street2."\n".$adressDetail."\n".$remark."\n".$lname."\n".$fname."\n".$sex."\n".$phone."\n".$email);
+        if($street1 == "選擇路段"){
+            $street1 = NULL;
+        }
+        if($street2 == "選擇路段"){
+            $street2 = NULL;
+        }
+
+        $create_time = date('Y-m-d H:i:s');
+        $missions = new Mission;
+        $missions->mission_content = $remark;
+        $missions->lname = $lname;
+        $missions->fname = $fname;
+        $missions->sex = $sex;
+        $missions->phone = $phone;
+        $missions->email = $email;
+        $missions->township_or_district_input = $township;
+        $missions->rd_or_st_1 = $street1;
+        $missions->rd_or_st_2 = $street2;
+        $missions->location = $adressDetail;
+        $missions->mission_list_id = 1;
+        $missions->created_at = $create_time;
+        $missions->updated_at = $create_time;
+        $missions->save();
+
+        $find_mission_id = DB::table('missions')
+            ->where('created_at',$create_time)
+            ->where('lname',$lname)
+            ->where('sex',$sex)
+            ->get();
+//        dd($find_mission_id);
+        $send_string1 = "向我們提供災情最新情況";
+        $send_string2 = "【C".(new Carbon($find_mission_id[0]->created_at))->formatLocalized('%y%m%d%H%M').$find_mission_id[0]->mission_id;
+        $send_string2 = $send_string2."】為您剛才通報的編號，您可以使用此編號在首頁查詢通報的處理情況";
+
+        return view('user_pages.submit_success')
+            ->with('string',$send_string1)
+            ->with('mission_id_string',$send_string2);
 	}
 
 	/**
--- resources/views/user_pages/submit_success.blade.php
+++ resources/views/user_pages/submit_success.blade.php
@@ -8,7 +8,14 @@
     <br><br><br><br><br>
     <center>感謝您{{ $string  }}</center>
     <br>
-    <center>5秒鐘之後將自動回到原本頁面</center>
+    @if( isset($mission_id_string) )
+        <center>{{ $mission_id_string  }}</center>
+        <br>
+        <center>請按<a href="input">返回</a>回到原本頁面</center>
+    @else
+        <center><div id="second"></div></center>
+    @endif
+
 
 @endsection
 
@@ -16,11 +23,19 @@
 @section('javascript')
     <script language="JavaScript1.2" type="text/javascript">
         function delayURL(url, time) {
-            setTimeout("top.location.href='" + url + "'", time);
+            document.getElementById('second').innerHTML = time/1000+"秒後將自動回到原本頁面";
+            if(time == 1000){
+                setTimeout("top.location.href='" + url + "'", time);
+            }
+            else{
+                setTimeout(function(){delayURL(url, time-1000)}, 1000);
+            }
         }
     </script>
     <script type="text/javascript">
-        delayURL("input", 5000);
+        if(document.getElementById('second').length != 0){
+            delayURL("input", 5000);
+        }
     </script>
 @endsection
 
